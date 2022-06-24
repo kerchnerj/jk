c29c30a213d946708ff8b08e697c01e7bc7ca31a
BCD15 <bruno69correadias@gmail.com>
2022-06-24 09:19:42 -0300

    alterar

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from '@/components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+
+      <nav>
+        <RouterLink to="/"></RouterLink>
+        <RouterLink to="/about"></RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+@import '@/assets/style.css';
+</style>
