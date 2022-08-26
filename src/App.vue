745ba4d3895104e04782084ac5a9c2ae76297439
Luiza Ribeiro <guhoepers25@gmail.com>
2022-08-26 13:03:02 -0300

    gls

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,14 @@ import { RouterLink, RouterView } from 'vue-router';
   <header>
     <div class="wrapper">
       <nav>
-        <RouterLink to="/"> Categorias</RouterLink>
-        <RouterLink to="/Livros"> Livros </RouterLink>
-        <RouterLink to="/Editoras"> Editoras </RouterLink>
-        <RouterLink to="/Autores"> Autores </RouterLink>
+        <RouterLink to="/">Categorias</RouterLink>
+        <RouterLink to="/Livros">Livros</RouterLink>
+        <RouterLink to="/Editoras">Editoras</RouterLink>
+        <RouterLink to="/Autores">Autores</RouterLink>
       </nav>
+      <div class="icon">
+        <i class='bx bx-log-in'></i>
+      </div>
     </div>
   </header>
 
@@ -21,4 +24,27 @@ import { RouterLink, RouterView } from 'vue-router';
 
 <style>
 @import '@/assets/style.css';
+header{
+  height: 30px;
+  background-color: white;
+  padding: 25px;
+}
+.icon {
+  float: right;
+  font-size: 30px;
+
+}
+a {
+  margin-right: 15px;
+  text-decoration: none;
+  font-size: 26px;
+  color: black;
+}
+a:hover{
+  text-decoration: underline;
+}
+body {
+  margin: 0;
+  padding: 0;
+}
 </style>
