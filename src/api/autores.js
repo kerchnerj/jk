fe551bb025b22f833b2ce3d4cb1d830c4cb98e1a
kerchnerj <jeniffekerchner12@gmail.com>
2022-09-14 08:27:05 -0300

    alteração

--- src/api/autores.js
+++ src/api/autores.js
@@ -1,28 +1,28 @@
 import axios from "axios";
 export default class AutoresApi {
   async buscarTodosOsAutores() {
-    const response = await axios.get("http://localhost:4000/autores");
+    const response = await axios.get("https://livraria-jeniffer.herokuapp.com/autores");
     return response.data;
   }
 
   async buscarAutores(id) {
-    const response = await axios.get(`http://localhost:4000/autores/${id}`);
+    const response = await axios.get(`https://livraria-jeniffer.herokuapp.com/autores/${id}`);
     return response.data;
   }
 
   async adicionarAutor(autor) {
-    const response = await axios.post("http://localhost:4000/autores", autor);
+    const response = await axios.post("https://livraria-jeniffer.herokuapp.com/autores", autor);
     return response.data;
   }
 
   async excluirAutor(id) {
-    const response = await axios.delete(`http://localhost:4000/autores/${id}`);
+    const response = await axios.delete(`https://livraria-jeniffer.herokuapp.com/autores/${id}`);
     return response.data;
   }
 
   async atualizarAutor(autor) {
     const response = await axios.put(
-      `http://localhost:4000/autores/${autor.id}`,
+      `https://livraria-jeniffer.herokuapp.com/autores/${autor.id}`,
       autor
     );
     return response.data;
--- src/api/categorias.js
+++ src/api/categorias.js
@@ -1,29 +1,29 @@
 import axios from "axios";
 export default class CategoriasApi {
   async buscarTodasAsCategorias() {
-    const response = await axios.get("http://localhost:4000/categorias");
+    const response = await axios.get("https://livraria-jeniffer.herokuapp.com/categorias");
     return response.data;
   }
   async buscarCategoria(id) {
-    const response = await axios.get(`http://localhost:4000/categorias/${id}`);
+    const response = await axios.get(`https://livraria-jeniffer.herokuapp.com/categorias/${id}`);
     return response.data;
   }
   async adicionarCategoria(categoria) {
     const response = await axios.post(
-      "http://localhost:4000/categorias",
+      "https://livraria-jeniffer.herokuapp.com/categorias",
       categoria
     );
     return response.data;
   }
   async excluirCategoria(id) {
     const response = await axios.delete(
-      `http://localhost:4000/categorias/${id}`
+      `https://livraria-jeniffer.herokuapp.com/categorias/${id}`
     );
     return response.data;
   }
   async atualizarCategoria(categoria) {
     const response = await axios.put(
-      `http://localhost:4000/categorias/${categoria.id}`,
+      `https://livraria-jeniffer.herokuapp.com/categorias/${categoria.id}`,
       categoria
     );
     return response.data;
